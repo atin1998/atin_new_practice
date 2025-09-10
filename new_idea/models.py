9a6c1b01bb1b91768d15ee5144a6fd348797cb5b
Atin Ray <atin.ray@shyamsteel.net>
2025-09-10 16:16:14 +0530

    data added

--- new_idea/models.py
+++ new_idea/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Member(models.model):
+class Member(models.Model):
     first_name=models.CharField(max_length=25)
     last_name=models.CharField(max_length=25)
 
